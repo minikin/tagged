701e79722b5d8e2b0b38dcfd2779b8c1e477547d
minikin <djminikin@gmail.com>
2023-06-17 11:02:15 +0200

    fix: clean up

--- test/src/tagged_test.dart
+++ test/src/tagged_test.dart
@@ -1,11 +1,1 @@
-// ignore_for_file: prefer_const_constructors
-import 'package:tagged/tagged.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('Tagged', () {
-    test('can be instantiated', () {
-      expect(Tagged(), isNotNull);
-    });
-  });
-}
+void main() {}
